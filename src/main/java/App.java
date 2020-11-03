6141bfc5bf521285cd09aa6f7306eed72a10d7f3
Michal <Michal@MICHAL-PC>
2020-11-03 20:29:34 +0100

    test commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("funguje");
 		System.out.println("funguje2");
+		System.out.println("funguje2");
+		System.out.println("funguje2");
 	}
 }
