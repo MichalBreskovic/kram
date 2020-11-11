c14ddfc0fce4736a6ee43fbc495be428f84512fa
Michal Breškovič <michal.breskovic@student.upjs.sk>
2020-11-11 10:58:13 +0100

    working test commit

--- java/src/main/java/kram/App.java
+++ java/src/main/java/kram/App.java
@@ -1,7 +1,25 @@
 package kram;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	
+	public void start(Stage stage) throws Exception {
+		Button button = new Button("stlaè ma!");
+		AnchorPane rootPane = new AnchorPane();
+		rootPane.getChildren().add(button);
+		rootPane.setPrefSize(400, 300);
+		Scene scene = new Scene(rootPane);
+		stage.setTitle("Hello World");
+		stage.setScene(scene);
+		stage.show();
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		launch(args);
 	}
 }
