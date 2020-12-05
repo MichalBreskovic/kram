35c7834bc9f9ae38c45b207b0405f0589beda8c1
Dell <Dell@DESKTOP-9UUQGG2>
2020-12-05 15:20:06 +0100

    Vytvorenie Welcome page

---
+++ java/src/main/java/WindowsControler/WelcomePageControler.java
@@ -0,0 +1,12 @@
+package WindowsControler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class WelcomePageControler {
+	@FXML
+	private Button hurrabutton;
+	 @FXML
+	private Button addButton;
+}
--- java/src/main/java/kram/App.java
+++ java/src/main/java/kram/App.java
@@ -1,18 +1,22 @@
 package kram;
 
+import WindowsControler.WelcomePageControler;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
+
+
 
 public class App extends Application {
 	
 	public void start(Stage stage) throws Exception {
-		Button button = new Button("stlaè ma!");
-		AnchorPane rootPane = new AnchorPane();
-		rootPane.getChildren().add(button);
-		rootPane.setPrefSize(400, 300);
+		WelcomePageControler controller = new WelcomePageControler();
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("WelcomePageCurrent.fxml"));
+		fxmlLoader.setController(controller);
+		Parent rootPane = fxmlLoader.load();
 		Scene scene = new Scene(rootPane);
 		stage.setTitle("Hello World");
 		stage.setScene(scene);
