8a665d36eebbbd137db361cfd37db25aaff629a7
Dell <fiksyk@gmail.com>
2020-12-11 20:56:01 +0100

    Update

--- java/src/main/java/WindowsControler/UserPageControler.java
+++ java/src/main/java/WindowsControler/UserPageControler.java
@@ -7,24 +7,13 @@ import kram.storage.subject.Subject;
 import kram.storage.subject.SubjectDao;
 import kram.storage.subject.SubjectDaoFactory;
 import kram.storage.user.User;
-import kram.storage.user.UserDao;
-import kram.storage.user.UserDaoFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
