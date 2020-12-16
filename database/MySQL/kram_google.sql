46085a238d75443d91cf520d74cd647916c2044f
Michal Breškovič <michal.breskovic@student.upjs.sk>
2020-12-16 19:39:55 +0100

    commit

--- database/MySQL/kram_google.sql
+++ database/MySQL/kram_google.sql
@@ -14,13 +14,13 @@ DROP TABLE IF EXISTS `subject`;
 
 CREATE TABLE `subject`(
 	subject_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	title VARCHAR(45) NOT NULL,
-	short VARCHAR(10)
+	title VARCHAR(45) NOT NULL UNIQUE,
+	short VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE `topic`(
 	topic_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	subject_id INT NOT null,
+	subject_id INT NOT NULL,
 	title VARCHAR(45) NOT NULL,
 	FOREIGN KEY(subject_id) REFERENCES `subject`(subject_id) ON DELETE CASCADE
 );
@@ -109,10 +109,14 @@ INSERT INTO `subject`(title, short) VALUES ("geografia", "GEO");
 INSERT INTO topic(subject_id, title) VALUES (1, "algebra");
 INSERT INTO topic(subject_id, title) VALUES (1, "analyza");
 
-INSERT INTO user(name, surname, username, password, teacher) VALUES("teacher", "teacher", "teacher", "teacher", 1);
-INSERT INTO user(name, surname, username, password, teacher) VALUES("user", "user", "user", "user", 0);
-INSERT INTO user(name, surname, username, password, teacher) VALUES("Janko", "Múdry", "janko", "12345", 0);
-INSERT INTO user(name, surname, username, password, teacher) VALUES("Jurko", "Hlúpy", "jurko", "54321", 0);
+-- heslo user
+INSERT INTO user(name, surname, username, password, teacher) VALUES("teacher", "teacher", "teacher", "1057a9604e04b274da5a4de0c8f4b4868d9b230989f8c8c6a28221143cc5a755", 1);
+-- heslo teacher
+INSERT INTO user(name, surname, username, password, teacher) VALUES("user", "user", "user", "04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb", 0);
+-- heslo 12345
+INSERT INTO user(name, surname, username, password, teacher) VALUES("Janko", "Múdry", "janko", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 0);
+-- heslo 12345
+INSERT INTO user(name, surname, username, password, teacher) VALUES("Jurko", "Hlúpy", "jurko", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 0);
 
 INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 5 = ", 1, 1);
 INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 6 = ", 1, 2);
@@ -140,4 +144,3 @@ INSERT INTO question_option(question_id, option_id, correct) VALUES(2,5, true);
 INSERT INTO `test`(user_id, topic_id, time_start, time_end, hodnotenie) VALUES(1, 1, "2020-12-15 21:21:15", "2020-12-21 08:00:00", "100");
 
 INSERT INTO answer(test_id, question_id, option_id) VALUES (1, 1, 1);
-
