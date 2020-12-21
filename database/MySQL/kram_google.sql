132a1374435d79f513fda6c80297c6818cb00c8a
Michal Breškovič <michal.breskovic@student.upjs.sk>
2020-12-21 14:59:56 +0100

    database commit

--- database/MySQL/kram_google.sql
+++ database/MySQL/kram_google.sql
@@ -27,10 +27,11 @@ CREATE TABLE `topic`(
 
 CREATE TABLE `user`(
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(45) NOT NULL,
     username VARCHAR(45) NOT NULL unique,
 	name VARCHAR(45) NOT NULL,
 	surname VARCHAR(45) NOT NULL,
-	password VARCHAR(64) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	teacher BOOLEAN
 );
 
@@ -52,7 +53,7 @@ CREATE TABLE `option`(
 
 CREATE TABLE `question_option`(
 	question_id INT NOT NULL,
-    option_id INT NOT NULL,
+    option_id INT,
     correct BOOLEAN,
     FOREIGN KEY(question_id) REFERENCES question (question_id) ON DELETE CASCADE,
     FOREIGN KEY(option_id) REFERENCES `option`(option_id) ON DELETE CASCADE,
@@ -61,8 +62,9 @@ CREATE TABLE `question_option`(
  
 CREATE TABLE `test`(
 	test_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(45),
 	user_id INT,
-    topic_id INT NOT NULL,
+    topic_id INT,
 	time_start datetime,
 	time_end datetime,
     hodnotenie int,
@@ -71,12 +73,12 @@ CREATE TABLE `test`(
 );
 
 CREATE TABLE `answer`(
+	answer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	test_id INT NOT NULL,
 	question_id INT NOT NULL,
-    option_id INT NOT NULL,
+    option_id INT,
     FOREIGN KEY (question_id , option_id) REFERENCES question_option (question_id , option_id) ON DELETE CASCADE,
-    FOREIGN KEY (test_id) REFERENCES test(test_id) ON DELETE CASCADE,
-	PRIMARY KEY (test_id, question_id, option_id)
+    FOREIGN KEY (test_id) REFERENCES test(test_id) ON DELETE CASCADE
 );
 
 CREATE TABLE `course`(
@@ -89,6 +91,7 @@ CREATE TABLE `course`(
 CREATE TABLE `course_user`(
 	course_id INT NOT NULL,
 	user_id INT NOT NULL,
+    accepted BOOL,
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES course(course_id) ON DELETE CASCADE,
 	PRIMARY KEY (user_id, course_id)
@@ -111,7 +114,6 @@ INSERT INTO `subject`(title, short) VALUES ("nemecký jazyk", "NEM");
 INSERT INTO `subject`(title, short) VALUES ("francúzky jazyk", "FRJ");
 INSERT INTO `subject`(title, short) VALUES ("prírodoveda", "PRV");
 INSERT INTO `subject`(title, short) VALUES ("dejepis", "DEJ");
-INSERT INTO `subject`(title, short) VALUES ("geografia", "GEO");
 INSERT INTO `subject`(title, short) VALUES ("chémia", "CHEM");
 INSERT INTO `subject`(title, short) VALUES ("biológia", "BIO");
 INSERT INTO `subject`(title, short) VALUES ("lekárstvo", "MED");
@@ -121,49 +123,58 @@ INSERT INTO topic(subject_id, title) VALUES (1, "algebra");
 INSERT INTO topic(subject_id, title) VALUES (1, "analyza");
 
 -- heslo user
-INSERT INTO user(name, surname, username, password, teacher) VALUES("teacher", "teacher", "teacher", "1057a9604e04b274da5a4de0c8f4b4868d9b230989f8c8c6a28221143cc5a755", 1);
+-- INSERT INTO user(name, surname, username, email, password, teacher) VALUES("teacher", "teacher", "teacher", "mail@gmail.com",  "1057a9604e04b274da5a4de0c8f4b4868d9b230989f8c8c6a28221143cc5a755", 1);
 -- heslo teacher
-INSERT INTO user(name, surname, username, password, teacher) VALUES("user", "user", "user", "04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb", 0);
+-- INSERT INTO user(name, surname, username, email, password, teacher) VALUES("user", "user",  "user", "mail@gmail.com",  "04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb", 0);
 -- heslo 12345
-INSERT INTO user(name, surname, username, password, teacher) VALUES("Janko", "Múdry", "janko", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 1);
+-- INSERT INTO user(name, surname, username, email, password, teacher) VALUES("Janko", "Múdry", "janko", "mail@gmail.com",  "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 1);
 -- heslo 12345
-INSERT INTO user(name, surname, username, password, teacher) VALUES("Jurko", "Hlúpy", "jurko", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 0);
+-- INSERT INTO user(name, surname, username, email, password, teacher) VALUES("Jurko", "Hlúpy", "jurko", "mail@gmail.com",  "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", 0);
 
-INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 5 = ", 1, 1);
-INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 6 = ", 1, 2);
-INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 7 = ", 1, 1);
+-- INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 5 = ", 1, 1);
+-- INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 6 = ", 1, 2);
+-- -- INSERT INTO question(title, topic_id, user_id) VALUES ("5 + 7 = ", 1, 1);
 
-INSERT INTO `option`(title) VALUES ("5");
-INSERT INTO `option`(title) VALUES ("6");
-INSERT INTO `option`(title) VALUES ("7");
-INSERT INTO `option`(title) VALUES ("10");
-INSERT INTO `option`(title) VALUES ("11");
-INSERT INTO `option`(title) VALUES ("12");
-INSERT INTO `option`(title) VALUES ("13");
-INSERT INTO `option`(title) VALUES ("14");
+-- -- INSERT INTO `option`(title) VALUES ();
+-- INSERT INTO `option`(title) VALUES ("5");
+-- INSERT INTO `option`(title) VALUES ("6");
+-- INSERT INTO `option`(title) VALUES ("7");
+-- INSERT INTO `option`(title) VALUES ("10");
+-- INSERT INTO `option`(title) VALUES ("11");
+-- INSERT INTO `option`(title) VALUES ("12");
+-- INSERT INTO `option`(title) VALUES ("13");
+-- INSERT INTO `option`(title) VALUES ("14");
 
-INSERT INTO question_option(question_id, option_id, correct) VALUES(1,1, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(1,2, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(1,3, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(1,4, true);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(1,1, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(1,2, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(1,3, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(1,4, true);
 
-INSERT INTO question_option(question_id, option_id, correct) VALUES(2,1, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(2,2, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(2,3, false);
-INSERT INTO question_option(question_id, option_id, correct) VALUES(2,5, true);
+-- -- INSERT INTO question_option(question_id, option_id, correct) VALUES(1,NULL, true); 
 
-INSERT INTO `test`(user_id, topic_id, time_start, time_end, hodnotenie) VALUES(1, 1, "2020-12-15 21:21:15", "2020-12-21 08:00:00", "100");
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(2,1, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(2,2, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(2,3, false);
+-- INSERT INTO question_option(question_id, option_id, correct) VALUES(2,5, true);
 
-INSERT INTO answer(test_id, question_id, option_id) VALUES (1, 1, 1);
-INSERT INTO answer(test_id, question_id, option_id) VALUES (3, 1, 2);
+-- INSERT INTO `test`(user_id, topic_id, time_start, time_end, hodnotenie) VALUES(2, 1, "2020-12-15 21:21:15", "2020-12-21 08:00:00", "100");
 
-INSERT INTO course(user_id, name) VALUES (1, "Operačné systémy");
-INSERT INTO course(user_id, name) VALUES (1, "Grafika");
-INSERT INTO course(user_id, name) VALUES (1, "Rýchlostné programovanie");
-INSERT INTO course(user_id, name) VALUES (3, "Diskrétna matematika");
+-- INSERT INTO answer(test_id, question_id, option_id) VALUES (1, 1, NULL);
+-- -- INSERT INTO answer(test_id, question_id, option_id) VALUES (1, 1, 1);
+-- -- INSERT INTO answer(test_id, question_id, option_id) VALUES (3, 1, 2);
 
-INSERT INTO course_user(course_id, user_id) VALUES (1,2);
-INSERT INTO course_user(course_id, user_id) VALUES (1,3);
-INSERT INTO course_user(course_id, user_id) VALUES (2,4);
+-- -- INSERT INTO answer(test_id, question_id, option_id) VALUES (1, 1, 1);-- 
 
-INSERT INTO course_test(course_id, test_id) VALUES (1,3);
+-- INSERT INTO course(user_id, name) VALUES (1, "Operačné systémy");
+-- INSERT INTO course(user_id, name) VALUES (1, "Grafika");
+-- INSERT INTO course(user_id, name) VALUES (1, "Rýchlostné programovanie");
+-- INSERT INTO course(user_id, name) VALUES (3, "Diskrétna matematika");
+
+-- INSERT INTO course_user(course_id, user_id, accepted) VALUES (1,2,1);
+-- INSERT INTO course_user(course_id, user_id, accepted) VALUES (2,2,1);
+-- INSERT INTO course_user(course_id, user_id, accepted) VALUES (1,3,1);
+-- INSERT INTO course_user(course_id, user_id, accepted) VALUES (2,4,0);
+-- -- INSERT INTO course_user(course_id, user_id, accepted) VALUES (2,4,0);
+
+-- INSERT INTO course_test(course_id, test_id) VALUES (1,1);
+-- -- INSERT INTO course_test(course_id, test_id) VALUES (1,2);
